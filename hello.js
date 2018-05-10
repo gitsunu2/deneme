40f3c27b717e560315c08b61320a79323e0b28ec
mysterion7 <grumpp@mail.com.tr>
2018-05-10 10:38:02 +0300

    help plz..

--- hello.js
+++ hello.js
@@ -1,7 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log("merhaba");
 console.log("merhaba dunya");
 console.log("merhaba yeni branch");
-=======
 console.log("merhaba");
->>>>>>> parent of 9802a6d... ikinci commit
+console.log("merhaba");
+console.log("merhaba yeni branch");
+console.log("merhaba");
+console.log("merhaba");
+
+
+
