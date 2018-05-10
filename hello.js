f9fdedddb74e964c6e8d8d62a070939698f3cdfa
mysterion7 <grumpp@mail.com.tr>
2018-05-10 10:35:04 +0300

    teşekkürler ramazan

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log("merhaba");
 console.log("merhaba dunya");
 console.log("merhaba yeni branch");
+=======
+console.log("merhaba");
+>>>>>>> parent of 9802a6d... ikinci commit
